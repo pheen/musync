4d9d2dbb1e10da27a68938928ab56b7cf8d8623b
Joel Korpela <syright@gmail.com>
2017-10-25 06:55:14 -0600

    switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171024095408) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groups", force: :cascade do |t|
     t.string "url", null: false
     t.integer "index"
